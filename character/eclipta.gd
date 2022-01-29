927c221cf2992869c8c8da9356f67cb603d99a4f
Antonio Ortiz <antonioortiz@MacBook-Pro-de-Antonio.local>
2022-01-29 18:38:21 -0400

    trancision de eclipse y ejecutables

--- character/eclipta.gd
+++ character/eclipta.gd
@@ -5,6 +5,7 @@ var velocity = Vector2(0,0)
 const GRAVITY = 35
 const SPEED = 200
 const JUMP = -900
+export var eclipse = false
 
 export var life = 3
 var saved_friends = 0
@@ -12,17 +13,29 @@ var saved_friends = 0
 func _physics_process(delta):
 	if Input.is_action_pressed("right"):
 		velocity.x = SPEED 
-		$AnimatedSprite.play("running")
+		if not eclipse:
+			$AnimatedSprite.play("running")
+		else:
+			$AnimatedSprite.play("running-night")	
 		$AnimatedSprite.flip_h = false
 	elif Input.is_action_pressed("left"):
 		velocity.x = -SPEED
-		$AnimatedSprite.play("running")
+		if not eclipse:
+			$AnimatedSprite.play("running")
+		else:
+			$AnimatedSprite.play("running-night")	
 		$AnimatedSprite.flip_h = true
 	else:
-		$AnimatedSprite.play("standing")	
+		if not eclipse:
+			$AnimatedSprite.play("standing")	
+		else:
+			$AnimatedSprite.play("standing-night")	
 	
 	if not is_on_floor():
-		$AnimatedSprite.play("jump")
+		if not eclipse:
+			$AnimatedSprite.play("jump")
+		else:
+			$AnimatedSprite.play("jump-night")
 		
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP
---
+++ levels/level-1.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+onready var timer = get_node("Timer")
+var bg_counter = 0
+
+const PHASES = 3
+
+func _ready():
+	$Timer.set_wait_time(4)
+	$Timer.start()
+
+
+func _on_Timer_timeout():
+	bg_counter += 1
+	bg_counter = 0 if bg_counter > PHASES else bg_counter
+	
+	if bg_counter == 0:
+		$ParallaxBackground/Sky/AnimatedSprite.play("sun-1")
+	if bg_counter == 1:
+		$ParallaxBackground/Sky/AnimatedSprite.play("sun-2")
+	if bg_counter == 2:
+		$ParallaxBackground/Sky/AnimatedSprite.play("sun-3")
+		$Player.eclipse = true
+	if bg_counter == 3:
+		$ParallaxBackground/Sky/AnimatedSprite.play("sun-2")
+		$Player.eclipse = false
